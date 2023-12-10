703b2362c39161aeddbbbc6360a0dab3a64cca03
Catherine Angel <catheriner40540@gmail.com>
2023-12-10 19:02:27 +0700

    update static settings

--- uibites/settings.py
+++ uibites/settings.py
@@ -155,6 +155,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
